61683386b757cef7f342e700db23505ad0c38095
xyfer <nav3071999@gmail,com>
2020-07-16 16:25:29 +0530

    Update smile_detection.py

--- smile_detection.py
+++ smile_detection.py
@@ -1,28 +1,39 @@
 import cv2 as cv
 
-simile_cas=cv.CascadeClassifier('haarcascade_smile.xml')
+# initializing the harrCascade file
+smile_cas=cv.CascadeClassifier('haarcascade_smile.xml')
 face_cas=cv.CascadeClassifier('haarcascade_smile.xml')
+
 
 def detect(gray, frame):
     faces = face_cas.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+        cv.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
         smiles = smile_cas.detectMultiScale(roi_gray, 1.8, 20)
 
         for (sx, sy, sw, sh) in smiles:
-            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+            cv.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
     return frame
 
 
-
+#capturing the video through the primary camera
 web=cv.VideoCapture(0)
 
 while True:
 
+    #Reading the captured video
     ret,frame=web.read()
     gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
 
     smile_detect = detect(gray,frame)
+
     cv.imshow('video',smile_detect)
+    # The control breaks once q key is pressed
+    if cv.waitKey(1) & 0xff==ord('q'):
+        break
+
+#release of the VideoCapture
+web.release()
+cv.destroyAllWindows()
